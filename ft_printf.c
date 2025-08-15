28ce8c111699ffd84e1cfcbd839dba73cd5aaf6c
FUZZAITI <pedrofuzzaiti@gmail.com>
2025-08-15 09:40:18 -0300

    norma

--- ft_printf.c
+++ ft_printf.c
@@ -1,52 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepinhei <pepinhei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 09:39:32 by pepinhei          #+#    #+#             */
+/*   Updated: 2025/08/15 09:39:33 by pepinhei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <unistd.h>
 
-static int    ft_putchar(char c)
+static int	ft_putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
-static int    ft_putstr(char *c)
+static int	ft_putstr(char *c)
 {
-  int i = 0;
+	int		i;
+
+	i = 0;
 	while (c[i] != '\0')
 	{
-	  (write(1, &c[i], 1));
-	  i++;
+		write(1, &c[i], 1);
+		i++;
 	}
+	return (i);
 }
 
-int    ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-  va_list    args;
-  
-  va_start(args, format);
-  
-  while (*format)
-  {
-	if (*format == '%')
+	va_list	args;
+	int		count;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
 	{
-	  format++;
-	  if (*format == 'c')
-		ft_putchar(va_arg(args, int));
-	  else if (*format == 's')
-		ft_putstr(va_arg(args, char *));
-	  else if (*format == '%')
-		ft_putchar('%');
-	  else
-	  {
-		ft_putchar('%');
-		format--;
-	  }
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += ft_putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += ft_putstr(va_arg(args, char *));
+			else if (*format == '%')
+				count += ft_putchar('%');
+			else
+			{
+				count += ft_putchar('%');
+				format--;
+			}
+		}
+		else
+			count += ft_putchar(*format);
+		format++;
 	}
-	else
-	  ft_putchar(*format);
-	format++;
-  }
-  va_end(args);
+	va_end(args);
+	return (count);
 }
 
-int main() {
-  ft_printf("Ola %Mundo %s com %s,%% %c",",tudo bem?","voce",'s');
-  return 0;
+int	main(void)
+{
+	ft_printf ("Ola %s com %s, %s", ",tudo bem", "voce ?", "sim, estou !");
+	return (0);
 }
